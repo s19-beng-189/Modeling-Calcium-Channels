f142f356d797c2ff0e7954128785967962d4bee5
Schmolesky <49256768+Schmolesky@users.noreply.github.com>
2019-05-29 15:18:32 -0700

    altered to account for small concentrations
    small concentrations of calcium should have a more drastic effect than large concentrations of calcium 
    used an additional equation to model this

--- betamCa.m
+++ betamCa.m
@@ -1,5 +1,9 @@
 function b=betamCa(v,Ca0)
 %filename: betam.m
+if Ca0>=1.1
 theta = (v+70)/18;
 theta2 = -0.916*exp(-0.046*Ca0)+0.908;
 b=4.0*exp(-theta) + theta2;
+else 
+    b=0.0239*Ca0+ 0.06236;
+end
