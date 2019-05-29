75d51552b1fc915d651e5ece172afee90ab3c437
Schmolesky <49256768+Schmolesky@users.noreply.github.com>
2019-05-29 09:52:29 -0700

    Initial code from model 2.1

---
+++ traub_parameters.m
@@ -0,0 +1,13 @@
+%max possible Na+ conductance per unit area:
+gNabar=100 %((muA/mV)/cm^2)
+%max possible K+ conductance per unit area:
+gKbar=80   %((muA/mV)/cm^2)
+%leakage conductance per unit area:
+gLbar=0.2  %((muA/mV)/cm^2)
+
+%Na+ equilibrium potential:
+ENa =  50   %(mV)
+%K+ equilibrium potential:
+EK = -100   %(mV)
+%leakage channel reversal potential:
+EL =  -67   %(mV)
