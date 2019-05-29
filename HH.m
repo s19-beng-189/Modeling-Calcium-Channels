9a6bf50ca98eabae41d60ba94fb6acb7a6a2de83
kbjegovi <49446152+kbjegovi@users.noreply.github.com>
2019-05-29 11:54:26 -0700

    Add files via upload

--- HH.m
+++ HH.m
@@ -25,8 +25,47 @@ for klok=1:klokmax
   end
   %store results for future plotting:
   mhn_plot(:,klok)=[m h n]';
+  g_plot(:,klok)=[gNa gK]';
   v_plot(klok)=v;
   t_plot(klok)=t;
 end
-subplot(2,1,1),plot(t_plot,v_plot)
-subplot(2,1,2),plot(t_plot,mhn_plot)
+
+in_mhnv
+for klok=1:klokmax
+  t=klok*dt;                      %note time
+  m=snew(m,alpham(v),betam(v),dt); %update m
+  h=snew(h,alphah(v),betah(v),dt); %update h
+  n=snew(n,alphan(v),betan(v),dt); %update n
+  gNa=gNabar*(m^3)*h;    %sodium conductance
+  gK =gKbar*(n^4);    %potassium conductance
+  gCa = gCabar*(m^2);
+  g=gNa+gK+gLbar+gCa;         %total conductance
+  gE=gNa*ENa+gK*EK+gLbar*EL+gCa*ECa;         %gE=g*E
+  %save old value of v for checking purposes:
+  v_old=v;
+  %update v:
+  v=(v+(dt/C)*(gE+izero(t)))/(1+(dt/C)*g);
+  if(check)
+    E=gE/g;
+    chv=C*(v-v_old)/dt+g*(v-E)-izero(t)
+  end
+  %store results for future plotting:
+  mhn_plot(:,klok)=[m h n]';
+  g_plotCa(:,klok)=[gNa gK gCa]';
+  v_plotCa(klok)=v;
+  t_plotCa(klok)=t;
+end
+figure(1)
+subplot(2,1,1)
+hold on
+plot(t_plot,v_plot)
+plot(t_plotCa,v_plotCa)
+title('Voltage vs. Time (no Ca2+)')
+xlabel('Time (ms)')
+ylabel('Voltage (mV)')
+legend('No Ca2+','Ca2+')
+subplot(2,1,2)
+hold on
+plot(t_plot,g_plot);
+plot(t_plotCa,g_plotCa);
+legend('gNa 1','gK1','gNa2','gK2','gCa');
--- in_HH.m
+++ in_HH.m
@@ -11,12 +11,26 @@
 %initialize membrane parameters:
 %membrane capacitance per unit area:
 C=1.0      %(muF/cm^2)
-%
-traub_parameters
+%max possible Na+ conductance per unit area:
+gNabar=120 %((muA/mV)/cm^2)
+%max possible K+ conductance per unit area:
+gKbar=36   %((muA/mV)/cm^2)
+%leakage conductance per unit area:
+gLbar=0.3  %((muA/mV)/cm^2)
+%max possible Ca2+ conductance per unit area:
+gCabar = 0.116; %((muA/mV)/cm^2) NEED TO FIND
+%Na+ equilibrium potential:
+ENa = 45   %(mV)
+%K+ equilibrium potential:
+EK = -82   %(mV)
+%leakage channel reversal potential:
+EL = -59   %(mV)
+%Ca2+ equilibrium potential:
+ECa = 134; %(mV) NEED TO FIND
 %
 %initialize time step and experiment duration:
 dt=0.1     %time step duration (ms)
-tmax=50    %duration of experiment (ms)
+tmax=15    %duration of experiment (ms)
 %total number of time steps in the experiment:
 klokmax=ceil(tmax/dt)
 %
@@ -33,13 +47,13 @@ t_plot=zeros(1,klokmax);
 %voltage prior to t=0:
 vhold=  -70  %(mV)
 %voltage just after t=0:
-vstart= -70  %(mV)
+vstart= -55  %(mV)
 %(change in v is result of current shock applied at t=0)
 %
 %initialize parameters of subsequent current pulse:
-t1p=0       %starting time (ms)
-t2p=tmax       %stopping time (ms)
-ip=1        %current applied (muA)
+t1p=10       %starting time (ms)
+t2p=11       %stopping time (ms)
+ip=0        %current applied (muA)
 %
 %initialize checking parameter
 check=1      %set check=1 to enable self-checking
