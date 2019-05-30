30f2cd4b22be4791c10ea0ab3adabcf37f07b90f
valerieporter <49161769+valerieporter@users.noreply.github.com>
2019-05-29 23:54:44 -0700

    HH Model with Calcium Channels and Sodium Inhibited Channels - Corrected
    corrected the initial m, and m_new value calculations, where f is the number of Na channels that are inhibited by calcium.  m_new is s_new but with f included in the calculation.

--- betamCa.m
+++ betamCa.m
@@ -1,9 +1,6 @@
 function b=betamCa(v,Ca0)
 %filename: betam.m
-if Ca0>=1.1
 theta = (v+70)/18;
-theta2 = -0.916*exp(-0.046*Ca0)+0.908;
+theta2 = (0.4651*log((0.574*Ca0.^(0.4071))+0.2874))+0.2951;
 b=4.0*exp(-theta) + theta2;
-else 
-    b=0.0239*Ca0+ 0.06236;
 end
---
+++ betamNa.m
@@ -0,0 +1,4 @@
+function b=betamNa(v)
+%filename: betam.m
+theta = (v+70)/18;
+b=2.0*exp(-theta);
