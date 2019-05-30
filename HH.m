886a03e9819207eccd687f56750999aa9228a956
kbjegovi <49446152+kbjegovi@users.noreply.github.com>
2019-05-30 14:53:22 -0700

    mCa -> m_Ca

--- HH.m
+++ HH.m
@@ -1,19 +1,17 @@
 %filename HH.m
 %numerical solution of the space-clamped Hodgkin-Huxley equations
 clear all
-clc
-close all
+clf
 global check;
 global t1p t2p ip; %parameters for the function izero(t)
-in_HH;
-in_mhnv;
-%Original Action Potential
+in_HH
+in_mhnv
 for klok=1:klokmax
   t=klok*dt;                      %note time
-  m_Ca=snew(m_Ca,alpham(v),betam(v),dt); %update m
+  m=snew(m,alpham(v),betam(v),dt); %update m
   h=snew(h,alphah(v),betah(v),dt); %update h
   n=snew(n,alphan(v),betan(v),dt); %update n
-  gNa=gNabar*(m_Ca^3)*h;    %sodium conductance
+  gNa=gNabar*(m^3)*h;    %sodium conductance
   gK =gKbar*(n^4);    %potassium conductance
   g=gNa+gK+gLbar;         %total conductance
   gE=gNa*ENa+gK*EK+gLbar*EL;         %gE=g*E
@@ -23,10 +21,10 @@ for klok=1:klokmax
   v=(v+(dt/C)*(gE+izero(t)))/(1+(dt/C)*g);
   if(check)
     E=gE/g;
-    chv=C*(v-v_old)/dt+g*(v-E)-izero(t);
+    chv=C*(v-v_old)/dt+g*(v-E)-izero(t)
   end
   %store results for future plotting:
-  mhn_plot_O(:,klok)=[m_Ca h n]';
+  mhn_plot(:,klok)=[m h n]';
   g_plot(:,klok)=[gNa gK]';
   v_plot(klok)=v;
   t_plot(klok)=t;
@@ -35,13 +33,13 @@ end
 in_mhnv
 for klok=1:klokmax
   t=klok*dt;                      %note time
-  m_Na=m_new(m_Na,alpham(v),betamNa(v),dt); %update m
+  m=snew(m,alpham(v),betamCa(v,Ca0),dt); %update m
   h=snew(h,alphah(v),betah(v),dt); %update h
   n=snew(n,alphan(v),betan(v),dt); %update n
-  mCa=snew(m_Ca,alpham(v),betam(v),dt);
-  gNa=gNabar*(m_Na^3)*h;    %sodium conductance
+  mCa=snew(m,alpham(v),betam(v),dt);
+  gNa=gNabar*(m^3)*h;    %sodium conductance
   gK =gKbar*(n^4);    %potassium conductance
-  gCa = gCabar*(mCa^2); %calcium conductance
+  gCa = gCabar*(mCa^2);
   g=gNa+gK+gLbar+gCa;         %total conductance
   gE=gNa*ENa+gK*EK+gLbar*EL+gCa*ECa;         %gE=g*E
   %save old value of v for checking purposes:
@@ -50,69 +48,38 @@ for klok=1:klokmax
   v=(v+(dt/C)*(gE+izero(t)))/(1+(dt/C)*g);
   if(check)
     E=gE/g;
-    chv=C*(v-v_old)/dt+g*(v-E)-izero(t);
+    chv=C*(v-v_old)/dt+g*(v-E)-izero(t)
   end
   %store results for future plotting:
-  mmhn_plot(:,klok)=[m_Na h n m_Ca]';
+  mhn_plot(:,klok)=[m h n]';
   g_plotCa(:,klok)=[gNa gK gCa]';
   v_plotCa(klok)=v;
   t_plotCa(klok)=t;
 end
 figure(1)
 hold on
-plot(t_plot(1:100),v_plot(1:100),'LineWidth',2)
-plot(t_plotCa(1:100),v_plotCa(1:100),'LineWidth',2)
-title('Action Potential')
+plot(t_plot,v_plot,'LineWidth',1)
+plot(t_plotCa,v_plotCa,'LineWidth',1)
+title('Voltage vs. Time (no Ca2+)')
 xlabel('Time (ms)')
 ylabel('Voltage (mV)')
-legend('Original HH Model','Ca2+ HH Model')
+legend('No Ca2+','Ca2+')
 
 figure(2)
-hold on
-plot(t_plot,v_plot,'LineWidth',2)
-plot(t_plotCa,v_plotCa,'LineWidth',2)
-title('Action Potential Spike Train')
-xlabel('Time (ms)')
-ylabel('Voltage (mV)')
-legend('Original HH Model','Ca2+ HH Model')
-
-figure(3)
 subplot(3,1,1)
 hold on
-plot(t_plot,g_plot(1,:),'LineWidth',2);
-plot(t_plotCa,g_plotCa(1,:),'LineWidth',2);
+plot(t_plot,g_plot(1,:),'LineWidth',1);
+plot(t_plotCa,g_plotCa(1,:),'LineWidth',1);
 title('Sodium Conductance');
-legend('gNa with no Ca2+','gNa with Ca2+');
+legend('gNa no Ca2+','gNa with Ca2+');
 subplot(3,1,2)
 hold on
-plot(t_plot,g_plot(2,:),'LineWidth',2);
-plot(t_plotCa,g_plotCa(2,:),'LineWidth',2);
+plot(t_plot,g_plot(2,:),'LineWidth',1);
+plot(t_plotCa,g_plotCa(2,:),'LineWidth',1);
 title('Potassium Conductance');
-legend('gK no with Ca2+','gK with Ca2+');
+legend('gK no Ca2+','gK with Ca2+');
 subplot(3,1,3)
 hold on
-plot(t_plotCa,g_plotCa(3,:),'LineWidth',2);
+plot(t_plotCa,g_plotCa(3,:),'LineWidth',1);
 title('Calcium Conductance');
 
-figure(4)
-subplot(2,1,1)
-hold on
-plot(t_plot, mhn_plot_O(1,:), 'LineWidth', 2);
-plot(t_plot, mhn_plot_O(2,:), 'LineWidth', 2);
-plot(t_plot, mhn_plot_O(3,:), 'LineWidth', 2);
-hold off
-title('Original HH Model Gating Variable Over Time')
-xlabel('Time (ms)')
-ylabel('Probability of Open Gates')
-legend('M Gates', 'H Gates', 'N Gates')
-subplot(2,1,2)
-hold on
-plot(t_plotCa, mmhn_plot(1,:), 'LineWidth', 2);
-plot(t_plotCa, mmhn_plot(2,:), 'LineWidth', 2);
-plot(t_plotCa, mmhn_plot(3,:), 'LineWidth', 2);
-plot(t_plotCa, mmhn_plot(4,:), 'LineWidth', 2);
-hold off
-title('New Calcium HH Model Gating Variable Over Time')
-xlabel('Time (ms)')
-ylabel('Probability of Open Gates')
-legend('Na M Gates', 'H Gates', 'N Gates', 'Ca M Gates')
