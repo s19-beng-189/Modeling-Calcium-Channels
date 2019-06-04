c356e1006447b63ad69a9b6f04a86a1aa6be56d9
valerieporter <49161769+valerieporter@users.noreply.github.com>
2019-06-04 09:35:27 -0700

    Updated bestfit-data.m
    Updated this file with the best fit curve info.

--- bestfit_data.m
+++ bestfit_data.m
@@ -1,9 +1,10 @@
 %Best fit line with sample data
 clc 
 clf
-ca0=[0 10 20 50];
-fraction= [0 0.3 0.57 0.81];
-f=-0.9164*exp(-0.046*ca0)+0.908; %goodness of fit line R^2 = 0.9859
+ca0=[0 0.2 2 10 20 50];
+fraction= [0 0.05 0.3 0.57 0.68 0.81];
+%Used Curve Fitting under Apps tab:
+f=(0.4651*log((0.574*ca0.^(0.4071))+0.2874))+0.2951; %goodness of fit line R^2 = 0.9979
 plot(ca0,fraction, 'o')
 hold on
 plot(ca0,f)
